78ec0e60fe378d74104266bc31972b522f60e8ad
Nazmul Hasan <nazmulcsharp@gmail.com>
2019-07-03 23:36:43 +0600

    Delete − Deletes an element using the given key..

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
  A linked list is a linear data structure:
  
  ====================================================================================
-A linked list is a linear data structure, in which the elements are not
+ A linked list is a linear data structure, in which the elements are not
  stored at contiguous memory locations. The elements in a linked list
  are linked using pointers.
  ------------------------------------------------------------------------------------
@@ -20,8 +20,8 @@ class Node{
     let value:Int
     var next : Node?
     init(value:Int, next : Node?) {
-      self.value = value
-      self.next = next
+        self.value = value
+        self.next = next
     }
 }
 
@@ -29,11 +29,11 @@ class LinkedList{
     var head : Node?
     
     //Setup − Dummy node.
-//    init() {
-//        let threeNode = Node(value: 3, next: nil)
-//        let twoNode = Node(value: 2, next: threeNode)
-//        head = Node(value: 1, next: twoNode)
-//    }
+    //    init() {
+    //        let threeNode = Node(value: 3, next: nil)
+    //        let twoNode = Node(value: 2, next: threeNode)
+    //        head = Node(value: 1, next: twoNode)
+    //    }
     
     //Insertion − Adds an element at the ending of the list.
     func insert(value : Int){
@@ -46,16 +46,32 @@ class LinkedList{
         while currentNode?.next != nil {
             currentNode = currentNode?.next
         }
-         currentNode?.next = Node(value: value, next: nil)
+        currentNode?.next = Node(value: value, next: nil)
     }
     
     //Delete − Delete an element from first..
     func DeleteFrist(){
         if head?.value != nil{
-          head = head?.next
+            head = head?.next
         }
     }
-
+    
+    //Delete − Deletes an element using the given key..
+    func Delete(value : Int){
+        if head?.value == value{
+            head = head?.next
+            return
+        }
+        
+        var prev : Node?
+        var currentNode = head
+        while currentNode?.value != value && currentNode != nil {
+            prev = currentNode
+            currentNode = currentNode?.next
+        }
+        prev?.next = currentNode?.next
+    }
+    
     //Display − Displays the complete list.
     func printList(){
         print("Printing out list of node")
@@ -75,5 +91,8 @@ ownLinkList.insert(value: 2)
 //1 -> 2 -> 3 -> nil
 ownLinkList.insert(value: 3)
 //2 -> 3 -> nil
-ownLinkList.DeleteFrist()
+//ownLinkList.DeleteFrist()
+//1 -> 3 -> nil
+ownLinkList.Delete(value: 2)
+
 ownLinkList.printList()
