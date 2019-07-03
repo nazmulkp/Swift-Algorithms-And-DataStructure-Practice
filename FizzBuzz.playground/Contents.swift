81b6e2b7f1dce2d524c448c8b60b1123f2e2134f
Nazmul Hasan <nazmulcsharp@gmail.com>
2019-07-03 08:57:03 +0600

    FizzBuzz apply on hundred number -Update Commnet

--- FizzBuzz.playground/Contents.swift
+++ FizzBuzz.playground/Contents.swift
@@ -4,14 +4,17 @@ import Foundation
 /*
  A typical round of Fizz Buzz can be:
  
- ====================================================================================================
- Write a program that prints the numbers from 1 to 100 and for multiples of '3' print "Fizz" instead of the number and for the multiples of '5' print "Buzz".
- ---------------------------------------------------------------------------------------------------
- 
+ ====================================================================================
+ Write a program that prints the numbers from 1 to 100 and
+ for multiples of '3' print "Fizz" instead of the number and
+ for the multiples of '5' print "Buzz".
+ ------------------------------------------------------------------------------------
+ Exmple:
+ ####################################################################################
  1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14,
  Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26,
  Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz, ...
- 
+ ####################################################################################
  */
 
 
