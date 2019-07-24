cdaeb69a744f12670a898efbbf8d365f1c2418ec
Nazmul Hasan <nazmulcsharp@gmail.com>
2019-07-24 10:27:28 +0600

    Implemanted Recurstion for  Factorial

--- FactorialsRecursion.playground/Contents.swift
+++ FactorialsRecursion.playground/Contents.swift
@@ -14,4 +14,14 @@ func factoriaOf(value : UInt)->UInt{
     return fact
 }
 
-print(factoriaOf(value: 1))
+//5x4x3x2x1
+func recursionFactoriaOf(value : UInt)->UInt{
+    //fact 0 mean 1 http://mathforum.org/library/drmath/view/57128.html
+    if value == 0 {
+        return 1
+    }
+    //print(value)
+    return value * recursionFactoriaOf(value: value - 1)
+}
+
+print(recursionFactoriaOf(value: 5))
