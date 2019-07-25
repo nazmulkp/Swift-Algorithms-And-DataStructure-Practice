6c25ade65bb632dfc69debc4ff60fb6148387d73
Nazmul Hasan <nazmulcsharp@gmail.com>
2019-07-25 09:01:15 +0600

    Fibonacci Sequence Done

--- FibonacciSequence.playground/Contents.swift
+++ FibonacciSequence.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
+func fibonacciSequenceNumberOf(steps : Int) ->[Int]{
+    var sequence = [0,1]
+    for _ in 0..<steps{
+        let first = sequence[sequence.count - 2]
+        let last = sequence.last!
+        
+        sequence.append(first + last)
+    }
+    return sequence
+}
 
+print(fibonacciSequenceNumberOf(steps: 10))
