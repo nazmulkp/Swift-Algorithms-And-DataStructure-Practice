82716b442de71922e0c881fbfd6caa44f886c420
Nazmul Hasan <nazmulcsharp@gmail.com>
2019-07-26 09:08:21 +0600

    Apply Recursion For Fibonacci Sequence Number Of Steps - U

--- FibonacciSequence.playground/Contents.swift
+++ FibonacciSequence.playground/Contents.swift
@@ -1,11 +1,16 @@
 import UIKit
 
 
+/*
+ Fibonacci Sequence. The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... The next number is found by adding up the two numbers before it.
+ */
+
 //test code area start
 
 let numberOfStep = 10
 
 //test code area end
+
 
 func fibonacciSequenceNumberOf(steps : Int) ->[Int]{
     var sequence = [0,1]
@@ -33,13 +38,8 @@ func recursionForFibonacciSequenceNumberOf(_ steps : Int,first : Int ,second:Int
     return [first + second] + recursionForFibonacciSequenceNumberOf(steps - 1, first: second, second: first+second)
 }
 
-
-
-
-
-
 print("######Fibonacci Sequence Number Of Steps######")
 print(fibonacciSequenceNumberOf(steps: numberOfStep))
 print("######Recursion For Fibonacci Sequence Number Of Steps######")
-print([0,1] + recursionForFibonacciSequenceNumberOf(numberOfStep,first: 0,second: 1)
+print(recursionForFibonacciSequenceNumberOf(numberOfStep,first: 0,second: 1)
 )
