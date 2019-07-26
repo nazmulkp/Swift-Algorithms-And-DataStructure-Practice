40309ecfd7e4e303da0bdc40d570008140d47ec6
Nazmul Hasan <nazmulcsharp@gmail.com>
2019-07-26 08:45:43 +0600

    Apply Recursion For Fibonacci Sequence Number Of Steps

--- FibonacciSequence.playground/Contents.swift
+++ FibonacciSequence.playground/Contents.swift
@@ -1,7 +1,17 @@
 import UIKit
 
+
+//test code area start
+
+let numberOfStep = 10
+
+//test code area end
+
 func fibonacciSequenceNumberOf(steps : Int) ->[Int]{
     var sequence = [0,1]
+    if steps == 0 {
+        return  []
+    }
     if steps <= 1{
         return sequence
     }
@@ -14,4 +24,22 @@ func fibonacciSequenceNumberOf(steps : Int) ->[Int]{
     return sequence
 }
 
-print(fibonacciSequenceNumberOf(steps: 1))
+
+func recursionForFibonacciSequenceNumberOf(_ steps : Int,first : Int ,second:Int) -> [Int]{
+    if steps == 0{
+      return []
+    }
+ 
+    return [first + second] + recursionForFibonacciSequenceNumberOf(steps - 1, first: second, second: first+second)
+}
+
+
+
+
+
+
+print("######Fibonacci Sequence Number Of Steps######")
+print(fibonacciSequenceNumberOf(steps: numberOfStep))
+print("######Recursion For Fibonacci Sequence Number Of Steps######")
+print([0,1] + recursionForFibonacciSequenceNumberOf(numberOfStep,first: 0,second: 1)
+)
